3c80055622c1624d354d14a1ef4b26d4758c4ddd
Beata Bloch <blochbeata.bb@gmail.com>
2019-04-09 22:02:13 +0200

    finished

--- task/settings.py
+++ task/settings.py
@@ -131,4 +131,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 django_heroku.settings(locals())
 
-# del DATABASES['default']['OPTIONS']['sslmode']
+del DATABASES['default']['OPTIONS']['sslmode']
